59b5bfa39085517b062fbdd677a61198f58e79b8
Lafaiete do Vale <lafa.vale@gmail.com>
2023-03-30 00:23:26 -0300

    Application componentized

---
+++ src/interfaces/contentProps.ts
@@ -0,0 +1,6 @@
+import { GenreResponseProps } from "./genreResponseProps";
+
+export interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
---
+++ src/interfaces/sideBarProps.ts
@@ -0,0 +1,4 @@
+export interface SideBarProps {
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
